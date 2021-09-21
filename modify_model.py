d7e5c235797f3a16b23111bd634de6cd01c7700d
Screamer <999screamer999@gmail.com>
2021-09-21 21:07:53 +0300

    rewrite service for update model

--- modify_model.py
+++ modify_model.py
@@ -1,8 +1,19 @@
+from keras.models import load_model
+
 import CONSTANT
 from agent.agent import Game_agent
 
-a = Game_agent()
-model2 = a.create_model()
-model2.load_weights(CONSTANT.AGENT_TEMP_MODEL_PATH, by_name=True)
-model2.save(CONSTANT.AGENT_TEMP_MODEL_PATH)
-a.get_bot().summary()
+just_get_summary = False
+save_modified_model = True
+
+if not just_get_summary:
+    a = Game_agent(CONSTANT.TENSORBOARD_LOG_DIR + "modify")
+    model2 = a.create_model()
+    model2.load_weights(CONSTANT.AGENT_TEMP_MODEL_PATH, by_name=True, skip_mismatch=True)
+    if save_modified_model:
+        model2.save(CONSTANT.AGENT_TEMP_MODEL_PATH)
+        load_model(CONSTANT.AGENT_TEMP_MODEL_PATH).summary()
+    else:
+        model2.summary()
+else:
+    load_model(CONSTANT.AGENT_TEMP_MODEL_PATH).summary()
